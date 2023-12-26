2963de99b5091cfc233c2cd25da880bb8fcf854a
Ayush Sharma <code.ayush@gmail.com>
2023-12-26 07:45:12 +0530

    Update _.sh
    Match the description of the script with the one mentioned in README.

--- _.sh
+++ _.sh
@@ -10,7 +10,9 @@
 #   installation of Arch Linux. It will then do
 #   everything that's needed to be done to recreate
 #   an OS with pretty and useful defaults, with
-#   choices for preferences wherever needed.
+#   choices for preferences wherever needed. After
+#   that's done, it'll act as a TUI to manage misc
+#   stuff related to OS: a manager for personalization.
 #
 #  ==============================================
 
