9158458f34a18e34df7759543ceeb4c17fd0c728
Ayush Sharma <code.ayush@gmail.com>
2023-12-26 07:25:54 +0530

    Initial Commit

---
+++ _.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#  ==============================================
+#
+#   ░░░░░░░░█▀▀░█░█
+#   ░░░░░░░░▀▀█░█▀█
+#   ░▀▀▀░▀░░▀▀▀░▀░▀
+#
+#   This script is supposed to run after minimal
+#   installation of Arch Linux. It will then do
+#   everything that's needed to be done to recreate
+#   an OS with pretty and useful defaults, with
+#   choices for preferences wherever needed.
+#
+#  ==============================================
+
+root=$(dirname "$(realpath "${BASH_SOURCE:-$0}")")
+
+function _ () {
+
+  #  ======================================
+  #   The very first thing that this script
+  #   must do is install every package that
+  #   this script itself relies upon (ex: gum)
+  #   and in order to do that, this script must
+  #   install an aur helper (like yay or paru)
+  #   so that it can install rest of the script
+  #   dependencies.
+  #  ======================================
+
+  return
+}
+
+_ "$@"
